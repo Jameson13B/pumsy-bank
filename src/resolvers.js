bd694f51f4025d9b32092e755ccd5099500f8d38
Jameson13B <jamesonbrown@neolivity.com>
2019-05-07 12:52:33 -0600

    First commit

---
+++ src/resolvers.js
@@ -0,0 +1,60 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { APP_SECRET, getUserId } = require('./utils');
+
+const resolvers = {
+  Query: {
+    users(root, args, cxt) {
+      return cxt.prisma.users();
+    },
+    user(root, args, cxt) {
+      return cxt.prisma.user({ id: args.id });
+    },
+    async usersClass(root, args, cxt) {
+      return cxt.prisma.users({ where: { class: args.class } });
+    }
+  },
+  Mutation: {
+    async createUser(root, args, cxt) {
+      const password = await bcrypt.hash(args.password, 10);
+      const user = await cxt.prisma.createUser({ ...args, password });
+      const token = jwt.sign({ userId: user.id }, APP_SECRET);
+      return { token, user };
+    },
+    async login(root, args, cxt) {
+      const user = await cxt.prisma.user({ email: args.email });
+      if (!user) throw new Error(`No user found with email: ${args.email}`);
+
+      const valid = await bcrypt.compare(args.password, user.password);
+      if (!valid) throw new Error('Invalid password');
+
+      const token = jwt.sign({ userId: user.id }, APP_SECRET);
+      return { token, user };
+    },
+    deleteUser(root, args, cxt) {
+      return cxt.prisma.deleteUser({ id: args.id });
+    },
+    updateUser(root, args, cxt) {
+      return cxt.prisma.updateUser({
+        where: { id: args.id },
+        data: {
+          name: args.name,
+          email: args.email,
+          class: args.class
+        }
+      });
+    }
+  },
+  User: {
+    log(root, args, ctx) {
+      return ctx.prisma.user({ id: root.id }).log();
+    }
+  },
+  Entry: {
+    postedBy(root, args, ctx) {
+      return ctx.prisma.entry({ id: root.id }).postedBy();
+    }
+  }
+};
+
+module.exports = resolvers;
